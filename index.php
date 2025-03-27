e9861f8c0f87f57fc2270627bf12e845ca3e4d76
fasilofficial <muhammedfasilofficial@gmail.com>
2025-03-27 16:25:30 +0530

    Handle CORS

--- index.php
+++ index.php
@@ -6,6 +6,15 @@ spl_autoload_register(function ($class) {
     require __DIR__ . "/src/{$class}.php";
 });
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 header("Content-Type: application/json");
 
 $id = $_GET['id'] ?? null;
